2033fec118e1d9cd86b48a699b4c681710fcaecb
진범 윤 <jinbeom4292@gmail.com>
2020-04-20 16:46:37 +0900

    초기 커밋.

---
+++ KakaoUrlExtractor/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace KakaoUrlExtractor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(12, 413);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(70, 25);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "열기";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(89, 413);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 25);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "저장";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(650, 388);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "사용 방법";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "카카오톡 URL 추출기";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ KakaoUrlExtractor/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Runtime.Serialization;
+using System.Windows.Documents;
+using System.Diagnostics;
+
+namespace KakaoUrlExtractor
+{
+    public partial class MainForm : Form
+    {
+        //List<URL> urlList = new List<URL>();
+        List<string> strList = new List<string>();
+        string url = " ";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenTxetFile();
+        }
+        private void OpenTxetFile()
+        {
+            OpenFileDialog txt = new OpenFileDialog();
+            txt.InitialDirectory = "C://";
+            txt.Filter = "텍스트 파일 (*.txt) | *.txt";
+
+            if (txt.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = txt.FileName;
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string text = " ";
+                        string firstText = sr.ReadLine();
+                        
+                        if (!firstText.Contains("카카오톡"))
+                        {
+                            MessageBox.Show("카카오톡 대화 파일이 아닙니다.", "ERROR!", MessageBoxButtons.OK);
+                            return;
+                        }
+                        while ((text = sr.ReadLine()) != null)
+                        {
+                            
+                            if (text.Length > 0 && text.StartsWith("["))
+                            {
+                                string[] temp = text.Split(new string[] { "] " }, StringSplitOptions.None);
+                                //MessageBox.Show(temp[0] + "\n" + temp[1], "ㅋㅋ", MessageBoxButtons.OK);
+                                if (temp[2].StartsWith("https://"))
+                                {
+                                    url = temp[2];
+                                    strList.Add(url);
+                                    listBox1.Items.Add(url);
+                                    //MessageBox.Show(url, "ㅋㅋ", MessageBoxButtons.OK);
+                                }
+                                else continue;
+                            }
+                            
+                        }
+                        
+                    }
+                }
+                
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void SaveFile()
+        {
+            using (FileStream fs = new FileStream("URL.txt", FileMode.Create))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+
+                for(int i = 0; i < strList.Count; i++)
+                {
+                    sw.WriteLine(strList[i], "\n");
+                }
+                sw.Close();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedUrl = listBox1.SelectedItem.ToString();
+            Clipboard.SetData(DataFormats.Text, (Object)selectedUrl);
+            Process.Start(selectedUrl);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("카카토오톡 PC 버전에서 대화 내보내기를 이용해 대화 내용을 추출합니다.\n" +
+                "텍스트 파일을 불러온 뒤 선택한 URL이 등록된 브라우저를 통해 열립니다.", "Help", MessageBoxButtons.OK);
+        }
+    }
+}
